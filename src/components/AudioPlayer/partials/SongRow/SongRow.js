e2a1addabd726df8f22b76a373c3508c46cdc8f9
Art92Gev <arturg995@gmail.com>
2024-01-28 21:48:32 +0400

    BugFix
    about duration song of NaN at first

--- src/components/AudioPlayer/partials/SongRow/SongRow.js
+++ src/components/AudioPlayer/partials/SongRow/SongRow.js
@@ -5,27 +5,31 @@ const SongRow = ({ currentSong, audioItem }) => {
 	const [seconds, setSeconds] = useState('00:00');
 	const [duration, setDuration] = useState('00:00');
 	const [rangeVal, setRangeVal] = useState(0);
-	const songRange = useRef();
 
 	useEffect(() => {
+
 		setInterval(() => {
-			if (audioItem.current) {
-				let currentMins = Math.floor(audioItem.current.currentTime / 60);
-				let currentSec = Math.floor(audioItem.current.currentTime - currentMins * 60);
-				let durationMins = Math.floor(audioItem.current.duration / 60);
-				let durationSec = Math.floor(audioItem.current.duration - durationMins * 60);
+			const updateDuration = () => {
+				if (audioItem.current && !isNaN(audioItem.current.duration)) {
+					let currentMins = Math.floor(audioItem.current.currentTime / 60);
+					let currentSec = Math.floor(audioItem.current.currentTime - currentMins * 60);
+					let durationMins = Math.floor(audioItem.current.duration / 60);
+					let durationSec = Math.floor(audioItem.current.duration - durationMins * 60);
+					currentMins = currentMins.toString().padStart(2, '0');
+					currentSec = currentSec.toString().padStart(2, '0');
+					durationMins = durationMins.toString().padStart(2, '0');
+					durationSec = durationSec.toString().padStart(2, '0');
 
-				currentMins = currentMins.toString().padStart(2, '0');
-				currentSec = currentSec.toString().padStart(2, '0');
-				durationMins = durationMins.toString().padStart(2, '0');
-				durationSec = durationSec.toString().padStart(2, '0');
-
-				setSeconds(currentMins + ':' + currentSec);
-				setDuration(durationMins + ':' + durationSec);
-				setRangeVal((audioItem.current.currentTime * 100) / audioItem.current.duration);
-			}
+					setSeconds(currentMins + ':' + currentSec);
+					setDuration(durationMins + ':' + durationSec);
+					setRangeVal((audioItem.current.currentTime * 100) / audioItem.current.duration);
+				}
+			};
+			updateDuration();
 		}, 1000);
-	}, [audioItem]);
+
+	}, [audioItem.current]);
+
 
 	const handleRangeChange = (e) => {
 		const newTime = (e.target.value * audioItem.current.duration) / 100;
@@ -39,7 +43,7 @@ const SongRow = ({ currentSong, audioItem }) => {
 					<img src={currentSong.picture} alt='' />
 					<div className='for-time'>
 						<p className='song-time'>{seconds}</p>
-						<input ref={songRange} type='range' className='song-range' value={rangeVal} max='100' onChange={handleRangeChange} />
+						<input type='range' className='song-range' value={rangeVal} max='100' onChange={handleRangeChange} />
 						<p className='song-duration'>{duration}</p>
 					</div>
 					<h2>{currentSong.name}</h2>
